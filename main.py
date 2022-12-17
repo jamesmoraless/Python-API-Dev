ed8e3446f6343b4f6c8d816469452f244d7451da
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2022-12-17 13:32:25 -0500

    postman setup

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/") #decorator turns root() to a path operation 
 def root_path():    
     return {"message": "Welcome to my API!!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
