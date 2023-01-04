f01bdd16530dfe9ae2cf8b33fd000927dec4141f
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2023-01-04 18:23:06 -0500

    adding post

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Python-API-Dev
-# Hello
 
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+#pip install fastapi[all]
+#uvicorn main:app to start the server 
+
+class Post(BaseModel):#Post object with strict and unscrict fields 
+    title: str
+    content: str
+    published: bool = True #if this value is not provided, default to true so make this optional
+    rating: Optional[int] = None #if not provided, give a value of 'None'
 
 
 @app.get("/") #decorator turns root() to a path operation 
@@ -10,3 +21,9 @@ def root_path():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(new_post: Post):#uses Pydantic to validate the data sent from the front-end
+    print(new_post)
+    print(new_post.dict())
+    return {"data": "new post"}
